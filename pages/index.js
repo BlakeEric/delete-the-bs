126369a1fc46ffdfb50d2ac9e50b0f3eaef801c3
Blake Lundquist <lundqub@outlook.com>
2019-07-13 12:53:52 -0700

    clean up

--- pages/index.js
+++ pages/index.js
@@ -22,30 +22,3 @@ const HomePage = () => (
 );
 
 export default HomePage;
-
-
-// const styles = css`
-//   .content {
-//     font-family: Georgia, Cambria, "Times New Roman", Times, serif;
-//     max-width: 700px;
-//     margin: auto;
-//     line-height: 1.5;
-//     min-height: 70vh;
-//     display: flex;
-//     justify-content: center;
-//     flex-direction: column;
-//   }
-//   .title {
-//     font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
-//     font-size: 54px;
-//     margin-bottom: 0;
-//   }
-//   .subTitle {
-//     font-weight: 500;
-//     color: gray;
-//     margin-top: 1rem;
-//   }
-//   .defaultText {
-//     text-align: center;
-//   }
-// `
